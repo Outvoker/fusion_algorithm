b8433948396bc55d9a0304d94581c066263de620
Outvoker <704457936@qq.com>
2020-12-15 07:32:25 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# 算法库开发
+
+## 环境
+
+### vscode远程python开发+Debug
+
+给远程服务器安装python插件
+
+配置远程服务器虚拟环境python解释器
+
+debug标签下run
+
+<https://fastapi.tiangolo.com/zh/tutorial/debugging/?h=+debug#run-your-code-with-your-debugger>
+
+### venv虚拟环境
+
+`python3 -m venv venv`
+
+进入虚拟环境
+
+`. ./venv/bin/activate`
+
+### FastAPI 文档
+
+<https://fastapi.tiangolo.com/>
+
+### Uvicorn 参数
+
+<https://www.uvicorn.org/deployment/>
+
+### 导出依赖
+
+`pip freeze > req.txt`
+
+安装依赖
+
+`pip install -r req.txt`
+
+## FastAPI数据库访问
+
---
+++ main.py
@@ -0,0 +1,50 @@
+import uvicorn
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from enum import Enum
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_price": item.price, "item_id": item_id}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+if __name__ == "__main__":
+    uvicorn.run(app='main:app', host="0.0.0.0", port=8000, reload=True)
